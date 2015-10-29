2d589370f301302dfef3bba9a33b7a6f910799fe
Andrew Fomera <andrew@zerlex.net>
2015-10-28 19:16:18 -0500

    SMS class, and make the basic send SMS from form work

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,5 +6,11 @@ class MessagesController < ApplicationController
 
   def create
     # Send the text here?
+    phone_number = params[:phone_number]
+    message_body = params[:body]
+
+    SMS.new(phone_number).send message_body
+
+    redirect_to root_path, notice: "Your message has been sent, thank you!"
   end
 end
---
+++ app/models/sms.rb
@@ -0,0 +1,17 @@
+class SMS
+  def initialize(to_phone_number)
+    @to_phone_number = to_phone_number
+  end
+
+  def client
+    Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_token
+  end
+
+  def send(body)
+    client.account.sms.messages.create(
+      :from => Rails.application.secrets.twilio_phone_number,
+      :to => @to_phone_number,
+      :body => body
+    )
+  end
+end
