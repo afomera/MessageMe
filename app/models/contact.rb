2ceedbd3da946491fdcdaddf582acbfba187dc96
Andrew Fomera <andrew@zerlex.net>
2016-02-10 00:00:44 -0600

    Contacts should now be used to send messages instead of entering phone numbers

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,8 @@ class Contact < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :first_name, :last_name, :phone_number
+
+  def full_name_with_phone
+    "#{first_name} #{last_name} - #{phone_number}"
+  end
 end
