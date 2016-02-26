751ba818b6f61cfe6c5a527c86635c9df32bb94f
Andrew Fomera <andrew@zerlex.net>
2016-02-25 19:20:55 -0600

    Rewrite unused sms job to use ActiveJob format, still unused job however

--- app/jobs/sms_job.rb
+++ app/jobs/sms_job.rb
@@ -1,9 +1,12 @@
-class SmsJob
-  include SuckerPunch::Job
+class SmsJob < ActiveJob::Base
+  queue_as :default
 
   def perform(phone_number, message_body)
-
+    # Send the SMS now, then update the message status
     SMS.new(phone_number).send message_body
-    #raise message
+
+    # Increase the users sent counter here when we add it.
+    # Then fire off the confirm job
   end
+
 end
