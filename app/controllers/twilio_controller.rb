d5711128f8057f5f102b97da09bf6cf8028684c2
Andrew Fomera <andrew@zerlex.net>
2016-02-27 18:58:21 -0600

    Pass in empty user info for twilio response

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -2,7 +2,7 @@ class TwilioController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    SMS.new(params[:From]).send("Thanks for messaging tmsg.io! We don't know how to handle your reply! For more help visit tmsg.io and 'Contact Support'")
+    SMS.new(params[:From], "").send("Thanks for messaging tmsg.io! We don't know how to handle your reply! For more help visit tmsg.io and 'Contact Support'")
     # the head :ok tells everything is ok
     head :ok, content_type: "text/html"
   end
